c6d68803507a69516a87e2f3142bec2cde07c4cf
jmohr@uvic.ca <jmohr@uvic.ca@d29389ee-23ba-95cd-0add-1ea859d12ca3>
2011-01-29 07:20:18 +0000

    Changed file load directory for linux

--- trunk/guano/math/Utility.h
+++ trunk/guano/math/Utility.h
@@ -1,36 +1,36 @@
 /****************************************
  * Various constants and utility functions
  * By Bill Perone (billperone@yahoo.com)
- * Original: 17-09-2003   
+ * Original: 17-09-2003
  * Revised:  05-12-2003
  *           12-12-2003
  *
  * Special thanks to Cbloom, Graphics Gems, Braeden Shosa
  *
- * © 2003, This code is provided "as is" and you can use it freely as long as 
+ * © 2003, This code is provided "as is" and you can use it freely as long as
  * credit is given to Bill Perone in the application it is used in
  *
- * Notes: 
+ * Notes:
  *    e^(i*pi) + 1 = 0
  *    e^(i*z) = cos(z) + i*sin(z)
  *    cos(z) = ( e^(i*z) + e^(-i*z) ) / 2
  *    sin(z) = ( e^(i*z) - e^(-i*z) ) / (2*i)
  *    sum(n=0 to k)(z^n) = ( 1-z^(k+1) ) / (1-z)
- *    sine rule: a/sin(A) = b/sin(B) = c/sin(C) 
+ *    sine rule: a/sin(A) = b/sin(B) = c/sin(C)
  *    cos(a)*cos(a) + sin(a)*sin(a) = 1
- *    sin(-t) = -sin(t) 
- *    cos(-t) = cos(t) 
+ *    sin(-t) = -sin(t)
+ *    cos(-t) = cos(t)
  *    tan(-t) = -tan(t)
- *    sin(pi-t) = sin(t) 
- *    cos(pi-t) = -cos(t) 
- *    tan(pi-t) = -tan(t) 
- *    sin(s+t) = sin(s)*cos(t) + cos(s)*sin(t) 
+ *    sin(pi-t) = sin(t)
+ *    cos(pi-t) = -cos(t)
+ *    tan(pi-t) = -tan(t)
+ *    sin(s+t) = sin(s)*cos(t) + cos(s)*sin(t)
  *    cos(s+t) = cos(s)*cos(t) - sin(s)*sin(t)
- *    sin(s-t) = sin(s)*cos(t) - cos(s)*sin(t) 
+ *    sin(s-t) = sin(s)*cos(t) - cos(s)*sin(t)
  *    cos(s-t) = cos(s)*cos(t) + sin(s)*sin(t)
- *    sin(2*t) = 2*sin(t)*cos(t) 
+ *    sin(2*t) = 2*sin(t)*cos(t)
  *    cos(2*t) = cos(2*t) - sin(2*t) = 2*cos(2*t) - 1 = 1 - 2*sin(2*t)
- *    sin(t/2) = ±sqrt((1 - cos(t)) / 2) 
+ *    sin(t/2) = ±sqrt((1 - cos(t)) / 2)
  *    cos(t/2) = ±sqrt((1 + cos(t)) / 2)
  *    sin(s) + sin(t) = 2 * sin((s+t)/2) * cos((s-t)/2)
  *    sin(s) - sin(t) = 2 * sin((s-t)/2) * cos((s+t)/2)
@@ -61,16 +61,16 @@ template<class Type> inline void Swap(Type &a,Type &b)
 // maximum of a or b
 #define MAX(a,b)    ( (a) > (b) ? (a) : (b) )
 
-// absolute value of a 
+// absolute value of a
 #define ABS(a)		(((a) < 0) ? -(a) : (a))
 
-// round a to nearest int 
+// round a to nearest int
 #define ROUND(a)	((a) > 0 ? (int)((a)+0.5) : -(int)(0.5-(a)))
 
-// take sign of a, either -1, 0, or 1 
-#define ZSGN(a)		(((a)<0) ? -1 : (a) > 0 ? 1 : 0)	
+// take sign of a, either -1, 0, or 1
+#define ZSGN(a)		(((a)<0) ? -1 : (a) > 0 ? 1 : 0)
 
-// take binary sign of a, either -1, or 1 if >= 0 
+// take binary sign of a, either -1, or 1 if >= 0
 #define SGN(a)		(((a)<0) ? -1 : 1)
 
 
@@ -80,12 +80,12 @@ template<class Type> inline void Swap(Type &a,Type &b)
 #define FLOAT_AS_INT(f)			(reinterpret_cast<const unsigned long &>(f))
 #define INT_AS_FLOAT(i)			(reinterpret_cast<const float &>(i))
 
-#define DBL_EPSILON     2.2204460492503131e-016 // smallest such that 1.0+DBL_EPSILON != 1.0 
-#define DBL_MAX         1.7976931348623158e+308 // max value 
-#define DBL_MIN         2.2250738585072014e-308 // min positive value 
-#define FLT_EPSILON     1.192092896e-07F        // smallest such that 1.0+FLT_EPSILON != 1.0 
-#define FLT_MAX         3.402823466e+38F        // max value 
-#define FLT_MIN         1.175494351e-38F        // min positive value 
+#define DBL_EPSILON     2.2204460492503131e-016 // smallest such that 1.0+DBL_EPSILON != 1.0
+#define DBL_MAX         1.7976931348623158e+308 // max value
+#define DBL_MIN         2.2250738585072014e-308 // min positive value
+#define FLT_EPSILON     1.192092896e-07F        // smallest such that 1.0+FLT_EPSILON != 1.0
+#define FLT_MAX         3.402823466e+38F        // max value
+#define FLT_MIN         1.175494351e-38F        // min positive value
 
 
 namespace Math
@@ -109,13 +109,13 @@ namespace Math
 	// ln(10)
 	const double LN10=     2.3025850929940456840179914546843642076011014886287729760333279009675726096773524802359972050895982983;
 	// ln(pi)
-	const double LNPI=     1.1447298858494001741434273513530587116472948129153115715136230714721377698848260797836232702754897077;	
+	const double LNPI=     1.1447298858494001741434273513530587116472948129153115715136230714721377698848260797836232702754897077;
 	// lg(e)
 	const double LOG2E=    1.44269504088896340736;
 	// log(e)
 	const double LOG10E=   0.434294481903251827651;
 	// ln(2)
-	const double LN2=      0.693147180559945309417;	
+	const double LN2=      0.693147180559945309417;
 	// sqrt(2)
 	const double SQRT2=    1.4142135623730950488016887242097;
 	// sqrt(3)
@@ -123,11 +123,11 @@ namespace Math
 
 
 	//! evaluates a degree (arraysize-1) polynomial in the order x^n*array[0] + ... + array[arraysize-1]
-	template <class type> 
+	template <class type>
 	inline type HornerEval(const float x, const type *array, unsigned int arraysize)
-	{			
+	{
 	    type p = *array;
-	    while (--arraysize) p= *++array + p*x;	
+	    while (--arraysize) p= *++array + p*x;
 		return p;
 	}
 
@@ -141,7 +141,7 @@ namespace Math
 	//! cosine interpolation of 2 points
 	template <class type>
 	inline interp_cos(const type &a, const type &b, float t)
-	{   
+	{
 		t = (1 - cos(t*PI))/2;
 	    return  a*(1-t) + b*t;
 	}
@@ -151,9 +151,9 @@ namespace Math
 	inline interp_cubic(const type &v0, const type &v1, const type &v2, const type &v3, float t)
 	{
 		type P = (v3 - v2) - (v0 - v1);
-		type Q = (v0 - v1) - P;	
+		type Q = (v0 - v1) - P;
 		return ((P*t + Q)*t + (v2-v0))*t + v1; //using expanded out horner's rule
-	}	
+	}
 
 
 	//! retuns the angle to side a given an arbitrary triangle of sides of length a,b,c
@@ -173,8 +173,8 @@ namespace Math
 		// getting one on purpose is very slim
 		//Assert(FLOAT_AS_INT(f) != 0xCDCDCDCD, "Probably incorrect float: 0xCDCDCDCD");
 		//Assert(FLOAT_AS_INT(f) != 0xDDDDDDDD, "Probably incorrect float: 0xDDDDDDDD");
-		
-		// this works because NAN always returns false on compares 
+
+		// this works because NAN always returns false on compares
 		return (f >= -FLT_MAX && f <= FLT_MAX);
 	}
 
@@ -183,7 +183,7 @@ namespace Math
 //	inline int round(const float f)
 //	{
 //		// Add a magical cookie to the float to transform its bits into its rounded integer representation
-//		// See http://www.d6.com/users/checker/pdfs/gdmfp.pdf		
+//		// See http://www.d6.com/users/checker/pdfs/gdmfp.pdf
 //		return (int)( (double)(f + 6755399441055744.L) );
 //	}
 
